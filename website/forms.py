fc424b0b5ae17d89db7ecbbcf206825a63e636ba
pedro <pedroxlbd@gmail.com>
2023-07-23 19:09:08 -0300

    remove db due to an issue and created the forms

---
+++ website/forms.py
@@ -0,0 +1,18 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField, BooleanField
+from wtforms.validators import DataRequired, Length, EqualTo, Email
+
+
+class RegistrationForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired(), Length(min=2, max=20)])
+    email = StringField('Email', validators=[Email(), DataRequired()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    confirm_password = PasswordField('Confirm Password', validators=[DataRequired(), EqualTo('password')])
+    submit = SubmitField('Sign up')
+    
+
+class LoginForm(FlaskForm):
+    email = StringField('Email', validators=[Email(), DataRequired()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    remember_me = BooleanField('Remember me')
+    submit = SubmitField('Log in')
--- website/routes.py
+++ website/routes.py
@@ -1,21 +1,27 @@
 from flask import render_template, redirect, url_for, flash
 from flask_login import login_manager, current_user, login_user, logout_user
 from website import db, app
-from website.models import User
+from website.forms import RegistrationForm, LoginForm
+
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
 
-@app.route('/login')
+@app.route('/login', methods=['GET', 'POST'])
 def login():
-    pass
+    form = LoginForm()
+    return render_template('login.html', form=form, title='Log in')
 
 @app.route('/logout')
 def logout():
     pass
 
-@app.route('/register')
+@app.route('/register', methods=['GET', 'POST'])
 def register():
-    pass
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        flash(f'Account created for {form.username.data}', 'success')
+        return redirect(url_for('index'))
+    return render_template('register.html', form=form, title='Register')
